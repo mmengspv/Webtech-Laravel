b5770d0234a2938aba245b83e5ebd4db287b9533
mmengspv <supavet.a@ku.th>
2021-09-10 00:08:49 +0700

    fix edit task date value

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -7,15 +7,15 @@
         @csrf
         <div>
             <label>Title : </label>
-            <input type="text" name="title" placeholder="title" autocomplete="off">
+            <input type="text" name="title" placeholder="title" autocomplete="off" required>
         </div>
         <div>
             <label>Detail : </label>
-            <input type="text" name="detail" placeholder="detail" autocomplete="off">
+            <input type="text" name="detail" placeholder="detail" autocomplete="off" required>
         </div>
         <div>
             <label>Due date : </label>
-            <input type="date" name="due_date" >
+            <input type="date" name="due_date" required >
         </div>
 
         <button type="submit">Add Task</button>
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -8,15 +8,15 @@
         @csrf
         <div>
             <label>Title : </label>
-            <input type="text" name="title" value="{{$task->title}}">
+            <input type="text" name="title" value="{{$task->title}}" required>
         </div>
         <div>
             <label>Detail : </label>
-            <input type="text" name="detail" value="{{$task->detail}}">
+            <input type="text" name="detail" value="{{$task->detail}}" required>
         </div>
         <div>
             <label for="due_date">Due_date : </label>
-            <input type="date" name="due_date" value="{{$task->due_date->format('Y-d-m')}}">       
+            <input type="date" name="due_date" value="{{$task->due_date->format('Y-m-d')}}" required>     
         </div>
         
         <button type="submit">Edit</button>
